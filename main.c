e5bd3f8a35c88fd123f370e93f9637b978e757d4
Arujan Raveendran <83099376+A101912@users.noreply.github.com>
2023-03-31 00:20:58 -0400

    Ripped it to streds
    Its now in structs

--- main.c
+++ main.c
@@ -2,13 +2,20 @@
 #include <string.h>
 #include <stdlib.h>
 
+typedef struct {
+    char refDate[500];
+    char geo[500];
+    char groupAge[500];
+    char sexGender[500];
+    char val[500];
+} Data;
+
 int main(void) {
     FILE *fp;
     char line[1000];
     char *token;
-    char refDate [10];
-    char geo[50], sexGender[20], groupAge[50];
-    char val [10];
+    int i = 0;
+    Data dataArray[1000];
 
     fp = fopen("statscan_diabetes.csv", "r");
 
@@ -17,37 +24,35 @@ int main(void) {
         return 1; // Exit program with error code
     }
 
-    fseek(fp, 0, SEEK_SET);
-    
-    fgets(line, sizeof(line), fp); // Read the first line (header) and discard it
+    fseek(fp, 0, SEEK_SET); // Points to the beginning of the file
 
     while (fgets(line, sizeof(line), fp)) {
 
         // Extracts reference date
         token = strtok(line, ",");
         if (token == NULL) continue;
-        strncpy(refDate, token, sizeof(refDate) - 1);
+        strncpy(dataArray[i].refDate, token, sizeof(dataArray[i].refDate) - 1);
 
         // Extracts province
         token = strtok(NULL, ",");
         if (token == NULL) continue;
-        strncpy(geo, token, sizeof(geo) - 1);
+        strncpy(dataArray[i].geo, token, sizeof(dataArray[i].geo) - 1);
 
         token = strtok(NULL, ",");
 
         // Extract group age
         token = strtok(NULL, ",");
         if (token == NULL) continue;
-        strncpy(groupAge, token, sizeof(groupAge) - 1);
+        strncpy(dataArray[i].groupAge, token, sizeof(dataArray[i].groupAge) - 1);
 
         // Extract gender
         token = strtok(NULL, ",");
         if (token == NULL) continue;
-        strncpy(sexGender, token, sizeof(sexGender) - 1);
+        strncpy(dataArray[i].sexGender, token, sizeof(dataArray[i].sexGender) - 1);
 
         // Skip 7 columns
-        int i;
-        for (i = 0; i < 8; i++) {
+        int j;
+        for (j = 0; j < 8; j++) {
             token = strtok(NULL, ",");
             if (token == NULL) continue;
         }
@@ -55,16 +60,21 @@ int main(void) {
         // Extracts Value
         token = strtok(NULL, ",");
         if (token == NULL) continue;
-        strncpy(val, token, sizeof(val) - 1);
+        strncpy(dataArray[i].val, token, sizeof(dataArray[i].val) - 1);
 
-        printf("REF date: %s\n", refDate);
-        printf("Geo: %s\n", geo);
-        printf("Group Age: %s\n", groupAge);
-        printf("Sex/Gender: %s\n", sexGender);
-        printf("Value: %s\n", val);
+        i++;
+    }
+
+    // Print out the data in the array of structs
+    for (int j = 0; j < i; j++) {
+        printf("REF date: %s\n", dataArray[j].refDate);
+        printf("Geo: %s\n", dataArray[j].geo);
+        printf("Group Age: %s\n", dataArray[j].groupAge);
+        printf("Sex/Gender: %s\n", dataArray[j].sexGender);
+        printf("Value: %s\n", dataArray[j].val);
         printf("\n");
     }
+
     fclose(fp);
     return 0;
 }
-
